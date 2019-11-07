82eb3f14805750d25118d81ce403f7113369efa1
Roger Aylagas <rogeraylagas97@gmail.com>
2019-11-07 16:01:14 +0100

    Added ChessBoard Constructor

--- src/main/java/edu/upc/etsetb/arqsoft/chess2019/server/ChessBoard.java
+++ src/main/java/edu/upc/etsetb/arqsoft/chess2019/server/ChessBoard.java
@@ -13,4 +13,21 @@ import java.util.HashMap;
  */
 public class ChessBoard {
     HashMap<String,Square> squares;
+
+    public ChessBoard() {
+        this.squares = new HashMap<String, Square>();
+        boolean[] odd_row = new boolean[]{true, false, true, false, true, false, true, false};
+        boolean[] even_row = new boolean[]{false, true, false, true, false, true, false, true};
+        for (int i = 1; i < 9; ++i) {
+            for (int j = 1; j < 9; ++j) {
+                if(i%2==0){
+                    this.squares.put(String.valueOf(i).concat(String.valueOf(j)), new Square(even_row[j]));
+                }else{
+                    this.squares.put(String.valueOf(i).concat(String.valueOf(j)), new Square(odd_row[j]));
+                }
+            }
+        }
+    }
+    
+    
 }
