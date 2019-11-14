e7ce801ee13696fc0967c25c0350a23aab9dc0f2
Roger Aylagas <rogeraylagas97@gmail.com>
2019-11-14 14:05:59 +0100

    Added Basic Check for a correct movement

--- src/main/java/edu/upc/etsetb/arqsoft/chess2019/server/King.java
+++ src/main/java/edu/upc/etsetb/arqsoft/chess2019/server/King.java
@@ -5,6 +5,8 @@
  */
 package edu.upc.etsetb.arqsoft.chess2019.server;
 
+import static java.lang.Math.abs;
+
 /**
  *
  * @author Roger Aylagas Torres
@@ -13,6 +15,15 @@ public class King extends Piece{
     
     public King(int init_row, int init_col, boolean color) {
         super(init_row, init_col, color);
+    }
+    
+    public boolean isPieceMovement(int rO, int cO, int rD, int cD){
+        int rm = abs(rO-rD);
+        int cm = abs(cO-cD);
+        if(!(rO-1<=rm && rm<=8-rO && cO-1<=cm && cm<=8-cO && (rm+cm!=0))) return false;
+        //TODO: Particular movement
+        
+        return true;
     }
     
 }
--- src/main/java/edu/upc/etsetb/arqsoft/chess2019/server/Knight.java
+++ src/main/java/edu/upc/etsetb/arqsoft/chess2019/server/Knight.java
@@ -5,6 +5,8 @@
  */
 package edu.upc.etsetb.arqsoft.chess2019.server;
 
+import static java.lang.Math.abs;
+
 /**
  *
  * @author Roger Aylagas Torres
@@ -15,6 +17,15 @@ public class Knight extends Piece{
     public Knight(int init_row, int init_col, boolean color, boolean side) {
         super(init_row, init_col, color);
         this.side = side;
+    }
+    
+    public boolean isPieceMovement(int rO, int cO, int rD, int cD){
+        int rm = abs(rO-rD);
+        int cm = abs(cO-cD);
+        if(!(rO-1<=rm && rm<=8-rO && cO-1<=cm && cm<=8-cO && (rm+cm!=0))) return false;
+        //TODO: Particular movement
+        
+        return true;
     }
     
 }
