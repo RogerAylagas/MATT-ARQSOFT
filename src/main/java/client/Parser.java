598f4c3d27d0362eb5975a518e1bc0bbfbcc4350
Roger Aylagas <rogeraylagas97@gmail.com>
2019-12-03 11:30:23 +0100

    Done identifyLinkedCells() in Parser class

--- src/main/java/client/Parser.java
+++ src/main/java/client/Parser.java
@@ -14,7 +14,22 @@ import java.util.ArrayList;
 public class Parser {
 
     ArrayList<String> identifyLinkedCells(String equation) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<String> linkedCells = new ArrayList<String>();
+        int len = equation.length();
+        for(int i = 0; i <= len; i++){
+            if(Character.isUpperCase(equation.charAt(i))){
+                String possibleLinkedCell = Character.toString(equation.charAt(i));
+                int j = i+1;
+                while(Character.isUpperCase(equation.charAt(j)) ||
+                        Character.isDigit(equation.charAt(j))){
+                    possibleLinkedCell.concat(Character.toString(equation.charAt(j)));
+                    ++j;
+                }
+                if(this.isOperator(Character.toString(equation.charAt(j))))
+                    linkedCells.add(possibleLinkedCell);
+            }
+        }
+        return linkedCells;
     }
 
     ArrayList<String> identifyFormulas(String equation) {
@@ -24,5 +39,15 @@ public class Parser {
     String shuntingYardAlgorithm(String equation) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    public boolean isOperator(String c){
+        String operatorChars = "/*-+";
+        if (operatorChars.contains(c)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
     
 }
