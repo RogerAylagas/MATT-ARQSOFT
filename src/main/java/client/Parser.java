099a1a2f686e544d9629266e37db65a4a1937c1a
Roger Aylagas <rogeraylagas97@gmail.com>
2019-12-24 13:56:55 +0100

    Finished SuntingYardAlgorithm method in class Parser

--- src/main/java/client/Parser.java
+++ src/main/java/client/Parser.java
@@ -5,7 +5,11 @@
  */
 package client;
 
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 /**
  *
@@ -13,7 +17,7 @@ import java.util.ArrayList;
  */
 public class Parser {
 
-    ArrayList<String> identifyLinkedCells(String equation) {
+    public ArrayList<String> identifyLinkedCells(String equation) {
         ArrayList<String> linkedCells = new ArrayList<String>();
         int len = equation.length();
         for(int i = 0; i < len; i++){
@@ -41,7 +45,7 @@ public class Parser {
     }
     
     
-    ArrayList<String> identifyFormulas(String equation) {
+    public ArrayList<String> identifyFormulas(String equation) {
         ArrayList<String> formulas = new ArrayList<String>();
         int len = equation.length();
         for(int i = 0; i < len; i++){
@@ -67,8 +71,63 @@ public class Parser {
         return formulas;
     }
 
-    String shuntingYardAlgorithm(String equation) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ArrayList<String> shuntingYardAlgorithm(String equation) throws InvalidSyntaxException{
+        ArrayList<String> stack = new ArrayList<String>();
+        ArrayList<String> queue = new ArrayList<String>();
+        boolean isParenthesis = false;
+        CharacterIterator it = new StringCharacterIterator(equation);
+        String lastInStack;
+        int idx;
+        
+        while (it.current() != CharacterIterator.DONE) {
+                char symbol =it.current();
+                if(isForStack( Character.toString(symbol))){
+                    if(!stack.isEmpty() && hasLessPrecedence(Character.toString(symbol),stack.get(stack.size()-1))){
+                        lastInStack = stack.remove(stack.size()-1);
+                        queue.add(lastInStack);
+                        while(hasLessPrecedence(Character.toString(symbol),stack.get(stack.size()-1))){
+                            lastInStack = stack.remove(stack.size()-1);
+                            queue.add(lastInStack);
+                        }
+                        stack.add(Character.toString(symbol));
+                    }else if(symbol=='('){
+                        stack.add(Character.toString(symbol));
+                        isParenthesis = true;
+                        
+                    }else if(symbol==')'){
+                        if(isParenthesis == false)
+                            throw new InvalidSyntaxException();
+                        else{
+                            ListIterator<String> lit = stack.listIterator(stack.size());
+                            while(!stack.get(lit.previousIndex()).equals("(")){
+                                idx = lit.previousIndex();
+                                lit.previous();
+                                lastInStack = stack.remove(idx);
+                                queue.add(lastInStack);
+                                
+                            }
+                            isParenthesis = false;
+                            idx = lit.previousIndex();
+                            lastInStack = stack.remove(idx);
+                        }    
+                    }else{
+                        stack.add(Character.toString(symbol));
+                    }
+                }else if(Character.isDigit(symbol)){
+                    queue.add(Character.toString(symbol));
+                }else{
+                    throw new InvalidSyntaxException();
+                }
+                it.next();
+        }
+        if(isParenthesis==true) System.out.println("syntax error");;
+        ListIterator<String> lit = stack.listIterator(stack.size());
+        String prev;
+        while(lit.hasPrevious()){
+            prev = lit.previous();
+            queue.add(prev);
+        }
+        return queue;
     }
     
     public boolean isOperator(String c){
@@ -79,6 +138,25 @@ public class Parser {
             return false;
         }
     }
+    
+    private boolean isForStack(String c){
+        String alowedChars = "/*-+()";
+        if (alowedChars.contains(c)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private boolean hasLessPrecedence(String in, String last) {
+        String highPrecedence = "/*";
+        String lowPrecedence = "+-";
+        if(lowPrecedence.contains(in) && highPrecedence.contains(last))
+            return true;
+        return false;
+                 
+    }
+    
     
     
 }
