6a1875ea9df66ec66f4f4149e37dacb1ae445655
Roger Aylagas <rogeraylagas97@gmail.com>
2019-12-24 18:32:51 +0100

    Done all required methods in BasicLib class

--- src/main/java/client/BasicLib.java
+++ src/main/java/client/BasicLib.java
@@ -5,6 +5,9 @@
  */
 package client;
 
+import java.util.Arrays;
+
+
 /**
  *
  * @author roger
@@ -15,4 +18,42 @@ public class BasicLib extends Library{
     }
     
     //TODO: Add required operations
+    public float suma(float a, float b){
+        return a+b;
+    }
+    
+    public float suma(float[] nums){
+        float res = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            res += nums[i];
+        }
+        return res;
+    }
+    
+    public float max(float[] nums){
+        Arrays.sort(nums);
+        return nums[nums.length-1];
+    }
+    public float max(float a, float b){
+        if(a>b) return a;
+        else return b;
+    }
+    
+    public float min(float a, float b){
+        if(a<b) return a;
+        else return b;
+    }
+    
+    public float min(float[] nums){
+        Arrays.sort(nums);
+        return nums[0];
+    }
+    
+    public float promedio(float a, float b){
+        return (float)(a+b)/2;
+    }
+    
+    public float promedio(float[] nums){
+        return (float)this.suma(nums)/nums.length;
+    }
 }
